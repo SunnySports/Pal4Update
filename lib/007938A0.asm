32c3990c6e23af13291325446898352f87c8e2b2
SunnySports <34559325+SunnySports@users.noreply.github.com>
2026-01-21 08:39:36 +0800

    新增直接加载资源文件的功能
    这项更新允许程序直接加载资源文件。
    
    例如当程序想要加载\gamedata\PALActor.cpk包里面的\104\T06.amf文件时，会先尝试加载\gamedata\PALActor\104\T06.amf文件，如果没有这个文件，才会去.cpk包里面寻找资源文件。
    
    这一版更新甚至允许加载原本在.cpk包中没有的文件。
    
    这项更新不适用于.smp包和它里面的.mp3文件。

--- lib/007938A0.asm
+++ lib/007938A0.asm
@@ -19,7 +19,7 @@
 
 @L00000001:
 	cmp dword ptr [ebp+0x1001B8], 0x07
-	jl short @L00000002
+	jl @ReadFromFile
 	push 0x1B7
 	push 0x8D6E50
 	push 0x8D7138
@@ -315,5 +315,208 @@
 	pop ebx
 	add esp, 0x30
 	ret 0x04
+
+!pad 90
+
+<009810F0..00981340>
+
+@ReadFromFile:
+	cmp dword ptr [ebp+0x4],0x2
+	jne @L00000002
+	sub esp, 0x208
+	mov edi, esp
+	xor eax, eax
+	mov ecx, 0x82
+	rep stosd ; 申请栈空间并填满00
+	lea eax, [ebp+0x1000B4]
+	lea edx, [esp+0x08]
+	push 0x100
+	push eax
+	push edx
+	call dword ptr [0x00A011D8] ; 复制cpk文件路径
+	add esp, 0x0C
+	lea edx, [esp+0x08]
+	push edx
+	call dword ptr [0x00A011B8] ; 转为小写
+	add esp, 0x04
+	lea edx, [esp+0x08]
+	push 0x8B4DE0
+	push edx
+	call dword ptr [0x00A011DC] ; 查找".cpk"
+	add esp, 0x08
+	test eax, eax
+	jnz short @L00000018 ; 没找到".cpk"，转回原代码
+	add esp, 0x208
+	jmp @L00000002
+
+@L00000018:
+	mov byte ptr [eax], 0x5C
+	mov byte ptr [eax+0x01], 0x00
+	lea eax, [esp+0x254]
+	lea edx, [esp+0x08]
+	push 0x100
+	push eax
+	push edx
+	call dword ptr [0x00A011D0] ; 拼接文件路径
+	add esp, 0x0C
+	lea ecx, [esp+0x08]
+	push 0x00
+	push 0x80
+	push 0x03
+	push 0x00
+	push 0x01
+	push 0x80000000
+	push ecx
+	call dword ptr [0x00841608] ; 打开文件
+	mov dword ptr [esp], eax
+	cmp eax, 0xFFFFFFFF
+	jnz short @L00000019
+	add esp, 0x208
+	jmp @L00000002 ; 打开文件失败，转回原代码
+
+@L00000019:
+	xor edx, edx
+	lea esi, [ebp+0x100088]
+	@@:mov ecx, dword ptr [esi]
+	cmp byte ptr [ecx], 0x00
+	jz short @f
+	inc edx
+	add esi, 0x04
+	cmp edx, 0x08
+	jl short @b
+	jmp short @L00000020
+	@@:mov esi, dword ptr [ebp+edx*4+0x100088]
+	test esi, esi
+	jnz short @L00000021
+
+@L00000020:
+	mov eax, dword ptr [esp] ; 无法打开更多文件
+	push eax
+	call dword ptr [0x008415E8]
+	add esp, 0x208
+	push @L00000019
+	push 0x8D6E50
+	push 0x8D7138
+	push 0x10
+	call 0x007941C0
+	add esp, 0x10
+	xor eax, eax
+	pop edi
+	pop esi
+	pop ebp
+	pop ebx
+	add esp, 0x30
+	ret 0x04
+
+@L00000021:
+	mov eax, dword ptr [esp]
+	push 0
+	push 0
+	push 0
+	push 2
+	push 0
+	push eax
+	call dword ptr [0x008415D4] ; 创建文件映射
+	mov dword ptr [esp+4], eax
+	test eax, eax
+	jnz short @L00000022
+	mov eax, dword ptr [esp] ; 创建文件映射失败
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件
+	add esp, 0x208
+	push @L00000021
+	push 0x8D6E50
+	push 0x8D6F04
+	push 0x10
+	call 0x007941C0
+	add esp, 0x10
+	xor eax, eax
+	pop edi
+	pop esi
+	pop ebp
+	pop ebx
+	add esp, 0x30
+	ret 0x04
+
+@L00000022:
+ 	call dword ptr [0x00841644]
+	cmp eax, 0xB7
+	jnz short @L00000023
+	mov eax, dword ptr [esp] ; 文件映射已经存在
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件
+	add esp, 0x208
+	push @L00000021
+	push 0x8D6E50
+	push 0x8D6EAC
+	push 0x10
+	call 0x007941C0
+	add esp, 0x10
+	xor eax, eax
+	pop edi
+	pop esi
+	pop ebp
+	pop ebx
+	add esp, 0x30
+	ret 0x04
+
+@L00000023:
+	mov edx, dword ptr [esp+4]
+	push 0x0
+	push 0x0
+	push 0x0
+	push 0x4
+	push edx
+	call dword ptr [0x008415D8] ; 获取文件映射视图
+	test eax, eax
+	jnz short @L00000024
+	mov eax, dword ptr [esp+4] ; 获取文件映射视图失败
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件映射
+	mov eax, dword ptr [esp]
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件
+	add esp, 0x208
+	push @L00000023
+	push 0x8D6E50
+	push 0x8D7108
+	push 0x10
+	call 0x007941C0
+	add esp, 0x10
+	xor eax, eax
+	pop edi
+	pop esi
+	pop ebp
+	pop ebx
+	add esp, 0x30
+	ret 0x04
+
+@L00000024:
+	xor edx, edx
+	or ecx, FFFFFFFF
+	mov byte ptr [esi], 0x1 ; 0=未打开，1=打开
+	mov dword ptr [esi+0x4], edx
+	mov dword ptr [esi+0x8], edx
+	mov dword ptr [esi+0xC], ecx
+	mov dword ptr [esi+0x10], eax ; 文件映射视图
+	mov dword ptr [esi+0x14], eax
+	mov dword ptr [esi+0x18], edx
+	mov byte ptr [esi+0x1C], dl ; 文件无需解压
+	mov dword ptr [esi+0x20], eax ; 文件内容
+	mov dword ptr [esi+0x28], edx
+	mov dword ptr [esi+0x2C], edx
+	mov eax, dword ptr [esp]
+	push 0x00
+	push eax
+	call dword ptr [0x00841564]
+	mov dword ptr [esi+0x24], eax ; 文件大小
+	mov eax, dword ptr [esp+4]
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件映射
+	mov eax, dword ptr [esp]
+	push eax
+	call dword ptr [0x008415E8] ; 关闭文件
+	add esp, 0x208
+	jmp @L00000016
 
 !pad 90
